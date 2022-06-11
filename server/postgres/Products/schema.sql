9aed36416ef8415a937786400d8ffeadee6c38c0
zebibg <zgebres@gmail.com>
2022-06-11 11:00:53 -0700

    Created schema

--- server/postgres/Products/schema.sql
+++ server/postgres/Products/schema.sql
@@ -2,17 +2,19 @@ DROP DATABASE IF EXISTS products WITH (FORCE);
 
 CREATE DATABASE products;
 
-DROP TABLE IF EXISTS products, features, styles, related, skus, photos;
-
+-- DROP TABLE IF EXISTS products, features, styles, related, skus, photos;
+-- DROP TABLE IF EXISTS styles, photos, skus;
+DROP TABLE IF EXISTS related;
 CREATE TABLE IF NOT EXISTS products (
   id INTEGER UNIQUE PRIMARY KEY NOT NULL,
   name VARCHAR(64) NOT NULL,
   slogan VARCHAR(128) NOT NULL,
   description TEXT NOT NULL,
   category VARCHAR(32) NOT NULL,
-  default_price INTEGER NOT NULL
+  default_price VARCHAR(16) NOT NULL
 );
 \copy products FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/product.csv' with (format csv,header true, delimiter ',');
+CREATE INDEX products_id_index ON products (id);
 
 CREATE TABLE IF NOT EXISTS features (
   id INTEGER UNIQUE PRIMARY KEY NOT NULL,
@@ -21,16 +23,18 @@ CREATE TABLE IF NOT EXISTS features (
   value VARCHAR(64) NOT NULL
 );
 \copy features FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/features.csv' with (format csv,header true, delimiter ',');
+CREATE INDEX features_id_index ON features (id);
 
 CREATE TABLE IF NOT EXISTS styles (
   id INTEGER UNIQUE PRIMARY KEY NOT NULL,
   product_id INTEGER NOT NULL,
   name VARCHAR(64) NOT NULL,
-  sale_price INTEGER NULL, 
-  original_price INTEGER NOT NULL,
-  default_style BOOLEAN NOT NULL DEFAULT FALSE
+  sale_price VARCHAR(32) NULL DEFAULT '0', 
+  original_price VARCHAR(32) NOT NULL,
+  default_style BOOLEAN  NULL DEFAULT FALSE
 );
-\copy styles FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/styles.csv' with (format csv,header true, delimiter ',', NULL 'null');
+\copy styles FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/styles.csv' with (format csv,header true, delimiter ',');
+CREATE INDEX style_id_index ON styles (id);
 
 CREATE TABLE IF NOT EXISTS related (
   id INTEGER UNIQUE PRIMARY KEY NOT NULL,
@@ -38,21 +42,27 @@ CREATE TABLE IF NOT EXISTS related (
   related_product_id INTEGER NOT NULL
 );
 \copy related FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/related.csv' with (format csv,header true, delimiter ',');
+CREATE INDEX related_id_index ON related (id);
 
+-- skus
 CREATE TABLE IF NOT EXISTS skus (
   id INTEGER UNIQUE PRIMARY KEY NOT NULL,
   styleId INTEGER NOT NULL,
   size VARCHAR(8) NOT NULL,
-  quantity INTEGER NOT NULL
+  quantity INTEGER NOT NULL,
+  FOREIGN KEY(styleID) REFERENCES "styles" (id)
 );
 \copy skus FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/skus.csv' with (format csv,header true, delimiter ',');
+-- CREATE INDEX skus_id_index ON skus (id);
 
 CREATE TABLE IF NOT EXISTS photos (
   id INTEGER UNIQUE PRIMARY KEY NOT NULL,
   styleId INTEGER NOT NULL,
   url TEXT NOT NULL,
-  thumbnail_url TEXT NOT NULL
+  thumbnail_url TEXT NOT NULL,
+  FOREIGN KEY(styleID) REFERENCES "styles" (id)
 );
 \copy photos FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/photos.csv' with (format csv,header true, delimiter ',');
+-- CREATE INDEX photos_id_index ON photos (id);
 -- psql -h localhost -U zebib -d test -c "\copy product FROM '/Users/Zebib/Documents/rfp/SDC/csvFiles/product.csv' with (format csv,header true, delimiter ',');"
 -- $ psql -h localhost -U zebib -d test -a -f /Users/Zebib/Documents/rfp/SDC/server/postgres/Products/schema.sql 
